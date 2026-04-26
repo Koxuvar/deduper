117151d7663f4c31529b9ecb745703ce79fc175f
Darkstar1 <connor@itsflux.net>
2026-04-26 00:24:28 -0500

    adding logic for args parsing

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,36 @@
 use sha2::{Digest, Sha256};
 use std::{
     collections::HashMap,
+    env::current_dir,
     fs::{ReadDir, read_dir},
     path::PathBuf,
 };
 
 fn main() {
+    //Grab user input in arguments
     let user_args: Vec<String> = std::env::args().collect();
 
-    //check for arg
-    if user_args.len() < 2 {
-        return;
-    }
-
-    //define var
-    let current_dir: &String = &user_args[1];
+    //Set directory to cwd if no arg is passed or 2 arg passed in args vec
+    let directory: String = if user_args.len() < 2 {
+        match current_dir() {
+            Ok(s) => match s.to_str() {
+                Some(s) => s.to_string(),
+                None => {
+                    println!("Error Occured");
+                    return;
+                }
+            },
+            Err(err) => {
+                println!("Error occured: {err}");
+                return;
+            }
+        }
+    } else {
+        user_args[1].clone()
+    };
 
     //go through all files in that dir and push them as DirEntry
-    let files_iter = match read_dir(current_dir) {
+    let files_iter = match read_dir(directory) {
         Ok(i) => i,
         Err(err) => {
             println!("Error with reading Directory: {err}");
@@ -29,17 +42,9 @@ fn main() {
     //hashed file contents are the key's
     let hmap = hasher(files_iter);
 
-    let mut counter: u8 = 0;
-    for (_k, v) in hmap {
-        if v.len() > 1 {
-            for p in v {
-                counter += 1;
-                let x = p.to_str().unwrap();
-                println!("{x}");
-            }
-        }
-    }
-    println!("Found {counter} duplicates:");
+    //Pass hmap into function that iterates over it and finds number of duplicates
+    //prints total number of duplicate file instances then lists the paths to those files
+    find_duplicates_and_print(hmap);
 }
 
 fn hasher(files_iter: ReadDir) -> HashMap<Vec<u8>, Vec<PathBuf>> {
@@ -58,10 +63,30 @@ fn hasher(files_iter: ReadDir) -> HashMap<Vec<u8>, Vec<PathBuf>> {
                 Err(_) => continue,
             };
             let hash_result = Sha256::digest(data).as_slice().to_vec();
-            let pbuf_vec = hmap.entry(hash_result).or_insert(Vec::new());
+            let pbuf_vec = hmap.entry(hash_result).or_default();
             pbuf_vec.push(file_path_buf);
         }
     }
 
     hmap
+}
+
+fn find_duplicates_and_print(hmap: HashMap<Vec<u8>, Vec<PathBuf>>) {
+    let mut counter: u8 = 0;
+    hmap.values().for_each(|v| {
+        if v.len() > 1 {
+            counter += 1
+        }
+    });
+
+    println!("Found {counter} duplicates:");
+
+    hmap.values().for_each(|v| {
+        if v.len() > 1 {
+            for p in v {
+                let x = p.to_str().expect("Non Unicode Chars in file path");
+                println!("{x}");
+            }
+        }
+    });
 }
