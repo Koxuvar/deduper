0621fd8e66387da707770823bbc755574549ca86
Darkstar1 <connor@itsflux.net>
2026-04-26 21:36:45 -0500

    install bash script added

--- install.sh
+++ install.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+set -e
 echo "Installing deduper..."
 
-cargo build --release
 
-wait $#
+cargo build --release
+mkdir -p ~/.local/bin
+cp target/release/deduper ~/.local/bin/deduper
+echo "Installed! File location: !/.local/bin/deduper"
